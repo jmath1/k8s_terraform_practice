cf29fc7b8dc0c301f3969f07e2ba11d6b736b5b1
Jonathan Math <jonathanamath@gmail.com>
2022-08-24 13:09:40 +0700

    add s3 backend

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "s3" {
+    bucket = "ycd-terraform-state-sandbox"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
