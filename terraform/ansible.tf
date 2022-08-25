4ff046489ff9f0bf6bf3b0f66eccc42cf9a951f3
Jonathan Math <jonathanamath@gmail.com>
2022-08-25 10:38:30 +0700

    fix ansible hosts template to provide hostnames

--- terraform/ansible.tf
+++ terraform/ansible.tf
@@ -1,13 +1,14 @@
  resource "local_file" "ansible_inventory" {
   content = templatefile("templates/inventory.tmpl",
     {
-      worker_ips = [
-        for instance in aws_instance.worker:
-          instance.public_ip 
-      ]
-      master_ips = [
+      masters = [
         for instance in aws_instance.master:
-          instance.public_ip 
+          "${instance.tags.Name}	ansible_host=${instance.public_ip}"
+
+      ]
+      workers = [
+        for instance in aws_instance.worker:
+          "${instance.tags.Name}	ansible_host=${instance.public_ip}"
       ]
     }
   )
