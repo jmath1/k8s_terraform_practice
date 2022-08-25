8ada4353f633cb395f542c0fa4cc5047eeeb68d1
Jonathan Math <jonathanamath@gmail.com>
2022-08-25 14:08:53 +0700

    continue work on k8s setup in ansible

--- terraform/ansible.tf
+++ terraform/ansible.tf
@@ -1,4 +1,4 @@
- resource "local_file" "ansible_inventory" {
+resource "local_file" "ansible_inventory" {
   content = templatefile("templates/inventory.tmpl",
     {
       masters = [
@@ -9,6 +9,23 @@
       workers = [
         for instance in aws_instance.worker:
           "${instance.tags.Name}	ansible_host=${instance.public_ip}"
+      ]
+    }
+  )
+  filename = "../ansible/inventory"
+}
+
+ resource "local_file" "hosts_file" {
+  content = templatefile("templates/hosts.tmpl",
+    {
+      masters = [
+        for instance in aws_instance.master:
+          "${instance.public_ip}  ${instance.tags.Name}"
+
+      ]
+      workers = [
+        for instance in aws_instance.worker:
+          "${instance.public_ip}  ${instance.tags.Name}"
       ]
     }
   )
