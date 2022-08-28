edea1cb1e4ea7ab6e59b583189090fae06c8788f
Jonathan Math <jonathanamath@gmail.com>
2022-08-28 17:03:57 +0700

    open 6443 on the ec2 instances so nodes can communicate

--- terraform/security.tf
+++ terraform/security.tf
@@ -23,6 +23,28 @@ resource "aws_security_group" "main" {
       security_groups  = []
       self             = false
       to_port          = 22
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 6443
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 6443
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 6443
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "udp"
+      security_groups  = []
+      self             = false
+      to_port          = 6443
     }
   ]
 }
